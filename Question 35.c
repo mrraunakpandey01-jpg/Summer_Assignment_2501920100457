0b6729421ec310bebff6d5c4d671a36828d291bf
Raunak kumar pandey <mrraunakpandey01@gmail.com>
2026-06-09 11:56:06 +0530

    Add files via upload

---
+++ Question 35.c
@@ -0,0 +1,25 @@
+/*Write a program to Print number triangle. 
+ * 
+ * A 
+ * B B 
+ * C C C 
+ * D D D D 
+ * E E E E E */
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    char ch = 'A';
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%c ",ch);
+        }
+        printf("\n");
+        ch++;
+    }
+
+    return 0;
+}
